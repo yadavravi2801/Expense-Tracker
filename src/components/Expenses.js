54745442895b0b621382a0c26f5d4775568f5fa3
ravi.yadav <ravi.yadav@angara.in>
2021-05-01 16:17:42 +0530

    Assignment 1 - React & Component Basics

---
+++ src/components/Expenses.js
@@ -0,0 +1,14 @@
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+        {
+            props.items.map(item => <ExpenseItem title={item.title} amount={item.amount} date={item.date} />)
+        }
+    </div>
+  );
+}
+
+export default Expenses;
