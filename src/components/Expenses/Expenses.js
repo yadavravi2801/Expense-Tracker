9e762c6d3a4a504e837393eed706318887394ee6
ravi.yadav <ravi.yadav@angara.in>
2021-05-08 19:12:30 +0530

    Work on state and events

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2022')
+
+  const filterChangeHandler = (year) => {
+    setFilteredYear(year);
+  }
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
         {
-            props.items.map(item => <ExpenseItem title={item.title} amount={item.amount} date={item.date} />)
+            props.items.map(item => <ExpenseItem key={item.title} title={item.title} amount={item.amount} date={item.date} />)
         }
     </Card>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
-    return <form>
+const ExpenseForm = (props) => {
+    const [userInput, setUserInput] = useState({
+        title : '',
+        amount : '',
+        date : ''
+    })
+
+    const changeHandler = (event) => {
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                [event.target.name] : event.target.value
+            }
+        })
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            ...userInput,
+            date : new Date(userInput.date)
+        }
+
+        props.onSaveExpenseData(expenseData);
+    }
+
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label htmlFor="">Title</label>
-                <input type="text" name="" id=""/>
+                <input type="text" name="title" value={userInput.title} onChange={changeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label htmlFor="">Amount</label>
-                <input type="number" name="" id="" min="0.01" step="0.01" />
+                <input type="number" name="amount" value={userInput.amount} min="0.01" step="0.01" onChange={changeHandler} />
             </div>
             <div className="new-expense__control">
                 <label htmlFor="">Date</label>
-                <input type="date" name="" id="" min="2019-01-01" max="2022-12-31" />
+                <input type="date" name="date" value={userInput.date} min="2019-01-01" max="2022-12-31" onChange={changeHandler} />
             </div>
         </div>
         <div className="new-expense__actions">
